c49c5c74ac58febd12f816e3abd1f61bf9329e86
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-04-09 20:10:06 -0500

    Initial commit

---
+++ src/spellchecker/SpellChecker.java
@@ -0,0 +1,24 @@
+package spellchecker;
+
+/*
+For this assignment, you will create a hash table to implement spell checker.
+1. Create a file of 100 words of varying length (max 8 characters).
+2. All the words are in lower case.
+3. design a hash table.
+4. enter each word in the hash table.
+Once the hash table is created, run it as a spell checker.  
+You enter a word (interactive), find the word in your hash table. 
+If not found enter an error message.  Then prompt for next word.  End the session by some control character like ctrl-c or something.
+1. use the linear probing first.  Count the number of collisions and print it.
+2. Then use quadratic probing.  Count the number of collisions and print it.
+3. Start with a table size that is about 53.  
+So 100 words would still have a load factor of less than 0.5  Now add 10 more words.  
+The program should automatically determine that table size needs to increase.
+Print out - increasing table size to <size>
+rehash all the old words also and the new words to the new hash table.
+You already have linear probing and quadratic probing functions.  Print the collisions in each case for the new table size.
+*/
+
+public class SpellChecker {
+
+}
